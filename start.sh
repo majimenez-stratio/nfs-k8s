c70fddbb01c312ab787030d8ea7e0d4d1c3bc5ff
majimenez-stratio <majimenez@stratio.com>
2021-06-01 20:30:19 +0200

    conf in start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,54 @@
 set -e
 
 # Options for starting Ganesha
-: ${GANESHA_LOGFILE:="/dev/stdout"}
 : ${GANESHA_OPTIONS:="-N NIV_EVENT"} # NIV_DEBUG
+: ${GANESHA_PROTOCOLS:="4"}
+: ${GANESHA_TRANSPORTS:="UDP,TCP"}
+: ${GANESHA_KRB5:="true"}
+: ${GANESHA_SECTYPE:="krb5"}
+
+function bootstrap_config {
+	echo "Bootstrapping Ganesha NFS config"
+  cat <<EOF > /usr/local/etc/ganesha/ganesha.conf
+NFSv4 {
+	Graceless = true;
+}
+NFS_KRB5 {
+	Active_krb5 = ${GANESHA_KRB5};
+}
+EXPORT {
+	# Export Id (mandatory, each EXPORT must have a unique Export_Id)
+	Export_Id = 1;
+
+	# Exported path (mandatory)
+	Path = /export;
+
+	# Pseudo Path (for NFS v4)
+	Pseudo = /;
+
+	# Access control options
+	Access_Type = RW;
+	Squash = No_Root_Squash;
+
+	# NFS protocol options
+	Transports = ${GANESHA_TRANSPORTS};
+	Protocols = ${GANESHA_PROTOCOLS};
+
+	SecType = ${GANESHA_SECTYPE};
+
+	# Exporting FSAL
+	FSAL {
+		Name = VFS;
+	}
+}
+EOF
+}
+
+function bootstrap_export {
+	if [ ! -f ${GANESHA_EXPORT} ]; then
+		mkdir -p "${GANESHA_EXPORT}"
+  fi
+}
 
 function init_rpc {
 	echo "Starting rpcbind"
@@ -22,8 +68,19 @@ function init_dbus {
 	sleep 1
 }
 
+function startup_script {
+	if [ -f "${STARTUP_SCRIPT}" ]; then
+  	/bin/sh ${STARTUP_SCRIPT}
+	fi
+}
+
+bootstrap_config
+bootstrap_export
+startup_script
+
 init_rpc
 init_dbus
+
 
 echo "Starting Ganesha NFS"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
